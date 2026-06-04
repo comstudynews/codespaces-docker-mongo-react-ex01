18cd22d1a04a2e38ce55ed111114987a14c8cb83
BEOMJOON KIM <96456853+comstudynews@users.noreply.github.com>
2026-06-04 07:01:53 +0000

    MongoDB Atlas와 Nodejs 연동

--- test/server_ex01.js
+++ test/server_ex01.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const todoRoutes = require("./routes/todoRoutes");
+const todoRoutes = require("../routes/todoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
